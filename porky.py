9f4a947c1d19833625817fc0b6f46cf5b5067021
John Hawthorn <john@freerunningtechnologies.com>
2015-03-17 17:11:53 -0700

    Switch to polling implementation
    Change-Id: I05bd9149b1cd90486869c1e9fc6d104ab706d505

--- porky.py
+++ porky.py
@@ -24,11 +24,22 @@ def awayButton(channel):
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(23, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
 GPIO.setup(24, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
-GPIO.add_event_detect(23, GPIO.RISING, callback=homeButton, bouncetime=1000)
-GPIO.add_event_detect(24, GPIO.RISING, callback=awayButton, bouncetime=1000)
+#GPIO.add_event_detect(23, GPIO.RISING, callback=homeButton, bouncetime=1000)
+#GPIO.add_event_detect(24, GPIO.RISING, callback=awayButton, bouncetime=1000)
+
+def waitForDown(pin):
+	while GPIO.input(pin):
+		sleep(0.05)
 
 while True:
-        print("1")
-        time.sleep(1)
+	if GPIO.input(23):
+		homeButton(23)
+		waitForDown(23)
+		time.sleep(0.5)
+	if GPIO.input(24):
+		awayButton(24)
+		waitForDown(24)
+		time.sleep(0.5)
+	time.sleep(0.05)
 
 GPIO.cleanup()
